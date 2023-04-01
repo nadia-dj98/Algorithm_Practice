c1b8ce0b387b18bf4e952af3bdfdb65b2638230a
nadia <nadia_dj@hotmail.co.uk>
2023-04-01 12:24:19 +0100

    solved first map problem

--- map_filter_reduce_code_exercises.js
+++ map_filter_reduce_code_exercises.js
@@ -2,4 +2,16 @@
 // Square the value of every element in the array. 
 //Presume that you will only get numbers in the input array.
 
-const input = [1, 2, 3, 4, 5]
+const input = [1, 2, 3, 4, 5];
+
+const squareValues = input.map(x => x**2);
+
+console.log(squareValues);
+
+//otherway
+
+const squareVals2 = input.map(function(num) {
+    return Math.pow(num, 2)
+})
+
+ console.log(squareVals2)
